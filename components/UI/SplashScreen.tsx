0463b6f42c05d10a60b89bd97a927948f8bc22a2
CJ <cjfritz9@gmail.com>
2023-10-17 17:19:15 -0600

    added splash screen variant for mobile/tablet

--- components/UI/SplashScreen.tsx
+++ components/UI/SplashScreen.tsx
@@ -18,21 +18,21 @@ const SplashScreen: React.FC = () => {
     const logoEle = logoRef.current;
     const screenHeight = window.innerHeight;
     const screenWidth = window.innerWidth;
-  
+
     const bgLoader = anime.timeline({
       targets: bgEle,
       complete: () => setShowSplash(false)
-    })
+    });
     const logoLoader = anime.timeline({
       targets: logoEle
-    })
+    });
 
     bgLoader.add({
-      duration: 400,
+      duration: 500,
       delay: 700,
       translateY: -screenHeight + 184,
       easing: 'linear'
-    })
+    });
 
     logoLoader.add({
       duration: 600,
@@ -44,7 +44,7 @@ const SplashScreen: React.FC = () => {
     logoLoader.add({
       delay: 100,
       duration: 300,
-      translateY: (screenHeight / 2) - 64,
+      translateY: screenHeight / 2 - 64,
       translateX: 48,
       scale: 1,
       easing: 'easeOutCirc'
@@ -53,16 +53,31 @@ const SplashScreen: React.FC = () => {
 
   useEffect(() => {
     animate();
+    scroll({behavior: 'smooth', top: 0})
   }, []);
 
   if (!showSplash || !isHome) return null;
 
   return (
-    <div ref={bgRef} className='flex fixed bg-[#1d232a] h-full w-full z-50 items-center overflow'>
-      <div ref={logoRef} className='translate-x-[-240px]'>
-        <Image priority src={logoAlt} alt='Full Blast Gear Logo'  height={76} />
+    <>
+      <div className='flex lg:hidden flex-col gap-12 top-4 left-4 sm:top-10 sm:left-12 absolute z-[51]'>
+        <Image priority src={logoAlt} alt='Full Blast Gear Logo' height={48} />
       </div>
-    </div>
+      <span className='block lg:hidden loading loading-spinner loading-lg text-white text-center absolute z-[52] top-[20dvh] left-[47dvw]'></span>
+      <div
+        ref={bgRef}
+        className='flex fixed bg-[#1d232a] h-full w-full z-50 items-center overflow'
+      >
+        <div ref={logoRef} className='hidden lg:block translate-x-[-240px]'>
+          <Image
+            priority
+            src={logoAlt}
+            alt='Full Blast Gear Logo'
+            height={76}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
