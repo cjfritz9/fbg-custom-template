318054eacd1b3a2e8ef714e6e5028c9a3387c3fe
CJ <cjfritz9@gmail.com>
2023-11-08 06:55:05 -0700

    fmhopefully final solution for verifiying webhook signature

--- app/webhooks/shopify-events/products/route.ts
+++ app/webhooks/shopify-events/products/route.ts
@@ -2,24 +2,26 @@ import { CACHE_TAG_PRODUCTS } from '@/app/api/requests';
 import { revalidateTag } from 'next/cache';
 import crypto from 'crypto';
 import { NextRequest } from 'next/server';
-import getRawBody from 'raw-body';
-import { Readable } from 'stream';
 
 export const POST = async (req: NextRequest) => {
-  const payload = await req.text();
+  const data = await req.text();
 
-  console.log({ payload });
-  console.log({ headers: req.headers.get('X-Shopify-Hmac-Sha256') });
-  const hash = crypto
+  console.log({ data });
+  const hmacHeader = req.headers.get('X-Shopify-Hmac-Sha256');
+  const digest = crypto
     .createHmac(
       process.env.HASH_HMAC_ALGORITHM!,
       process.env.SHOPIFY_WEBHOOK_SIGNATURE!
     )
-    .update(payload)
-    .digest('base64');
-  console.log({ hash });
-
-  revalidateTag(CACHE_TAG_PRODUCTS);
-
-  return new Response(null, { status: 200 });
+    .update(data)
+    .digest(process.env.HASH_HMAC_ENCODING! as crypto.BinaryToTextEncoding);
+  
+  if (hmacHeader === digest) {
+    revalidateTag(CACHE_TAG_PRODUCTS);
+    return new Response(null, { status: 200 });
+  } else {
+    return new Response('Unauthorized Request', {
+      status: 401
+    })
+  }
 };
