ac89234e6835782b8b04b451ec50b0efcc7f4d80
CJ <cjfritz9@gmail.com>
2023-11-08 06:47:59 -0700

    .

--- app/webhooks/shopify-events/products/route.ts
+++ app/webhooks/shopify-events/products/route.ts
@@ -6,20 +6,20 @@ import getRawBody from 'raw-body';
 import { Readable } from 'stream';
 
 export const POST = async (req: NextRequest) => {
-  const payload = await req.json();
+  const payload = await req.text();
 
   console.log({ payload });
   console.log({ headers: req.headers.get('X-Shopify-Hmac-Sha256') });
-  // const hash = crypto
-  //   .createHmac(
-  //     process.env.HASH_HMAC_ALGORITHM!,
-  //     process.env.SHOPIFY_WEBHOOK_SIGNATURE!
-  //   )
-  //   .update(payload.body, 'utf8')
-  //   .digest('base64');
-  // console.log({ hash });
+  const hash = crypto
+    .createHmac(
+      process.env.HASH_HMAC_ALGORITHM!,
+      process.env.SHOPIFY_WEBHOOK_SIGNATURE!
+    )
+    .update(payload)
+    .digest('base64');
+  console.log({ hash });
 
   revalidateTag(CACHE_TAG_PRODUCTS);
 
-  return new Response(null, { status: 204 });
+  return new Response(null, { status: 200 });
 };
