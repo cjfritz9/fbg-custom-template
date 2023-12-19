467515e2b5dc2a61a5d68af0fa670eeb12366c4c
CJ <cjfritz9@gmail.com>
2023-12-19 14:41:36 -0500

    added calendar .ics event for launch day

--- components/UI/DevDisclaimerModal.tsx
+++ components/UI/DevDisclaimerModal.tsx
@@ -1,13 +1,23 @@
 'use client';
 
 import React, { useEffect } from 'react';
+import { IoMdDownload } from 'react-icons/io';
 
 const DevDisclaimerModal: React.FC = () => {
+  const handleDownload = () => {
+    const pdfUrl = 'LaunchDayEvent.ics';
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = '/public/LaunchDayEvent.ics';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   useEffect(() => {
     //@ts-ignore
-    document.getElementById('dev-disclaimer').showModal()
-  }, [])
+    document.getElementById('dev-disclaimer').showModal();
+  }, []);
 
   return (
     <>
@@ -19,8 +29,20 @@ const DevDisclaimerModal: React.FC = () => {
             </button>
           </form>
           <h3 className='font-bold text-lg'>Welcome to Full Blast!</h3>
-          <p className='py-4'>This website is currently under development and is being used for testing purposes only.</p>
-          <p className='py-4'>Please join us when the site goes live January 15th!</p>
+          <p className='py-4'>
+            This website is currently under development and is being used for
+            testing purposes only.
+          </p>
+          <p className='py-4'>
+            Please join us when the site goes live January 15th!
+          </p>
+          <span
+            className='flex gap-2 text-lg items-center cursor-pointer py-4 text-blue-600'
+            onClick={handleDownload}
+          >
+            <p className=''>Remind Me</p>
+            <IoMdDownload />
+          </span>
         </div>
       </dialog>
     </>
