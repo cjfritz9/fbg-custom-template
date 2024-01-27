882f8294a1437284972088826065d3a5c5c60444
CJ <cjfritz9@gmail.com>
2024-01-26 19:02:51 -0500

    added server log for webhook req text

--- app/webhooks/shopify-events/middleware.ts
+++ app/webhooks/shopify-events/middleware.ts
@@ -1,7 +1,6 @@
 import crypto from 'crypto';
 import { NextRequest } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
   const data = await req.text();
 
@@ -27,6 +26,6 @@ export async function middleware(req: NextRequest) {
 }
 
 // See "Matching Paths" below to learn more
-export const config = {
-  matcher: '/webhooks/shopify-events/:path*'
-};
+// export const config = {
+//   matcher: ['/webhooks/shopify-events/:path*', '/webhooks/shopify-events/:path*']
+// };
