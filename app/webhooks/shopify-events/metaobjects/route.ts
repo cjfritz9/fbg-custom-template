38c64c47c4ec45641a73034c3a73daadec11e18b
CJ <cjfritz9@gmail.com>
2024-01-26 19:44:13 -0500

    added server log for webhook req text

--- app/webhooks/shopify-events/metaobjects/route.ts
+++ app/webhooks/shopify-events/metaobjects/route.ts
@@ -3,29 +3,9 @@ import { revalidateTag } from 'next/cache';
 import { NextRequest } from 'next/server';
 import crypto from 'crypto';
 
-export const POST = async (req: NextRequest) => {
-  const data = await req.text();
-
-  console.log(data);
-
-  console.info('Webhook received...');
-  const hmacHeader = req.headers.get('X-Shopify-Hmac-Sha256');
-  const digest = crypto
-    .createHmac(
-      process.env.HASH_HMAC_ALGORITHM!,
-      process.env.SHOPIFY_WEBHOOK_SIGNATURE!
-    )
-    .update(data)
-    .digest(process.env.HASH_HMAC_ENCODING! as crypto.BinaryToTextEncoding);
-
-  if (hmacHeader === digest) {
-    console.info('Webhook validated...');
-    console.info('Metaobjects cache updating.');
-    revalidateTag(CACHE_TAG_METAOBJECTS);
-    return new Response(null, { status: 200 });
-  } else {
-    return new Response('Unauthorized Request', {
-      status: 401
-    });
-  }
+export const POST = async (_req: NextRequest) => {
+  console.info('Webhook validated...');
+  console.info('Metaobjects cache updating.');
+  revalidateTag(CACHE_TAG_METAOBJECTS);
+  return new Response(null, { status: 200 });
 };
